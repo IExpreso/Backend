a544f2269bb8e3d05ed584ecb9dd9eede3808e4a
Hermes Espínola <hermesespinola@gmail.com>
2017-10-16 22:10:14 -0500

    resolve users model conflict

--- models/user.js
+++ models/user.js
@@ -1,55 +1,13 @@
-'use strinct';
+'use strict';
 
-<<<<<<< HEAD
-const models  = require('../models');
-const express = require('express');
-const router  = express.Router();
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcrypt');
-
-const env = process.env.NODE_ENV || 'development';
-
-const SALT_FACTOR = 5;
-let hashPassword = new Promise((password) => {
-  bcrypt.genSalt(SALT_FACTOR, (err, salt) => {
-    if (err)
-      return password;
-    bcrypt.hash(password, salt, null, (err, hash) => {
-      if (err)
-        return password;
-      return hash;
-    });
-  });
-});
-
-=======
->>>>>>> working
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     email: {type: DataTypes.STRING, primaryKey: true, validate: {
       isEmail: {msg: 'Reason'}
     }},
     password: {type: DataTypes.STRING, allowNull: false},
-    resetPasswordToken: { type: DataTypes.STRING },
-    resetPasswordExpires: { type: DataTypes.STRING },
     createdAt: {type: DataTypes.DATE, defaultValue: sequelize.fn('now')},
     updatedAt: {type: DataTypes.DATE, defaultValue: sequelize.fn('now')}
-  });
-
-  User.beforeCreate((user, options) => {
-    return hashPassword(user.password).then(hash => {
-      user.password = hash;
-    });
-  });
-
-  User.beforeValidate((user, options) => {
-    return hashPassword(user.password).then(hash => {
-      user.password = hash;
-    });
   });
 
   User.associate = function(models) {
