e2167e74a74430d0f01ae55a57407abb7161a155
Hermes Espínola <hermesespinola@gmail.com>
2017-10-18 20:31:29 -0500

    Add routes info to db

--- config/create_db.sql
+++ config/create_db.sql
@@ -44,3 +44,113 @@ INSERT INTO Alumns(id, name, startDate, endDate) VALUES(
   '2015-05-02 13:05:22',
   '2020-10-13 13:05:22'
 );
+INSERT INTO Routes(name) VALUES('Tránsito'), ('Chapultepec'), ('Cañadas'), ('Ciudadela'), ('Santa Anita'), ('Guadalupe'), ('Bugambilias'), ('Sur 2');
+
+ -- RUTA TRÁNSITO
+INSERT INTO Stops(name, time) VALUES('Tránsito',	'05:40'),
+                                 ('Circunvalación y Ávila Camacho',	'05:45'),
+                                 ('Plaza Patria',	'05:50'),
+                                 ('Glorieta Colón',	'05:54'),
+                                 ('López Mateos y Manuel Acuña',	'06:00'),
+                                 ('Hospital Terranova',	'06:02'),
+                                 ('Restaurante La Mar',	'06:05'),
+                                 ('Soriana Pablo Neruda',	'06:07'),
+                                 ('Glorieta Olímpica',	'06:10'),
+                                 ('Chedraui',	'06:15');
+
+-- RUTA CHAPULTEPEC
+INSERT INTO Stops(name, time) VALUES('Glorieta Chapultepec', '05:40'),
+                                 ('Vallarta y Progreso',	'05:44'),
+                                 ('Centro Magno',	'05:48'),
+                                 ('Plaza Bonita',	'05:55'),
+                                 ('Plaza Galerías',	'06:02'),
+                                 ('Naciones Unidas y Patria',	'06:07'),
+                                 ('Plaza Universidad',	'06:10'),
+                                 ('Paseo Madrigal',	'06:11'),
+                                 ('Paseo Royal Country',	'06:13'),
+                                 ('Plaza Pabellón',	'06:15');
+
+-- RUTA CAÑADAS
+INSERT INTO Stops(name, time) VALUES('Cañadas',	'06:05'),
+                                 ('Punto San Isidro',	'06:08'),
+                                 ('Valle de San Isidro',	'06:10'),
+                                 ('Jabil',	'06:15');
+
+-- RUTA SUR
+INSERT INTO Stops(name, time) VALUES('El Palomar', '05:20'),
+                                 ('Bugambilias', '05:30'),
+                                 ('Las Fuentes', '05:37'),
+                                 ('Las Águilas', '05:40'),
+                                 ('La Calma', '05:43'),
+                                 ('Plaza del Sol', '05:48'),
+                                 ('Hotel Hilton',	'05:53'),
+                                 ('Glorieta Chapalita',	'06:00'),
+                                 ('Guadalupe y Niño Obrero',	'06:02'),
+                                 ('Guadalupe y San Nicolás de Bari',	'06:05'),
+                                 ('Guadalupe y Patria',	'06:08'),
+                                 ('Toreros',	'06:10'),
+                                 ('SRE',	'06:12'),
+                                 ('Super La Playa',	'06:15'),
+                                 ('Guadalupe y Santo Tomás',	'06:16'),
+                                 ('Guadalupe y San Lorenzo',	'06:18'),
+                                 ('Ciudad Judicial',	'06:25');
+
+-- RUTA CIUDADELA
+INSERT INTO Stops(name, time) VALUES('Plaza Ciudadela',	'06:00'),
+                                 ('Cordilleras',	'06:05'),
+                                 ('Patria y Beethoven',	'06:07'),
+                                 ('Patria y Sebastian Bach',	'06:19'),
+                                 ('Galerías',	'06:13'),
+                                 ('Los Tules',	'06:20');
+
+-- RUTA SANTA ANITA
+INSERT INTO Stops(name, time) VALUES('XÓCHITL Y NETZAHUALCÓYOTL',	'06:22'),
+                                 ('Cuahutémoc y Mariano Otero',	'06:25'),
+                                 ('López  Mateos Y Pegaso',	'06:30'),
+                                 ('López  Mateos Y Orión',	'06:33'),
+                                 ('López Mateos Y Copérnico',	'06:35'),
+                                 ('López  Mateos Y Las Fuentes',	'06:38'),
+                                 ('Bugambilias',	'06:46'),
+                                 ('El Palomar',	'06:56'),
+                                 ('San José el Tajo',	'07:00'),
+                                 ('NUEVA GALICIA',	'07:02'),
+                                 ('San Martín el Tajo',	'07:07'),
+                                 ('Manantial',	'07:10'),
+                                 ('Club de Golf Santa Anita',	'07:12');
+
+-- RUTA GUADALUPE
+INSERT INTO Stops(name, time) VALUES('Glorieta Chapalita', '06:15'),
+                                 ('Guadalupe y Niño Obrero',	'06:18'),
+                                 ('Guadalupe  y San Nicolás de Bari',	'06:20'),
+                                 ('Guadalupe y Patria',	'06:22'),
+                                 ('Guadalupe y Toreros',	'06:24'),
+                                 ('Plaza Alegra',	'06:26'),
+                                 ('Super La Playa',	'06:28'),
+                                 ('Guadalupe y Santo Tomás',	'06:29'),
+                                 ('Guadalupe y San Lorenzo',	'06:30'),
+                                 ('Ciudad Judicial',	'06:38');
+
+-- RUTA BUGAMBILIAS
+INSERT INTO Stops(name, time) VALUES('Palomar',	'05:55'),
+                                 ('Bugambilias',	'06:06'),
+                                 ('Las Fuentes',	'06:16'),
+                                 ('Las Águilas',	'06:19'),
+                                 ('Copérnico y Clouthier',	'06:26'),
+                                 ('Mariano Otero y Jesús S. Carrillo',	'06:30');
+
+-- RUTA SUR 2
+INSERT INTO Stops(name, time) VALUES('Bugambilias',	'06:35'),
+                                 ('Las Fuentes',	'06:45'),
+                                 ('Las Águilas',	'06:48'),
+                                 ('La Calma',	'06:51'),
+                                 ('Plaza del Sol',	'06:57'),
+                                 ('Plaza del Ángel',	'07:00'),
+                                 ('Glorieta Chapalita',	'07:06'),
+                                 ('Guadalupe y Niño Obrero',	'07:10'),
+                                 ('Guadalupe/s Nicolás de Bari',	'07:12'),
+                                 ('Guadalupe y Patria',	'07:15'),
+                                 ('Toreros',	'07:18'),
+                                 ('SRE',	'07:21'),
+                                 ('Super la Playa',	'07:24'),
+                                 ('Guadalupe y Santo Tomás',	'07:25'),
+                                 ('Guadalupe y San Lorenzo',	'07:26');
