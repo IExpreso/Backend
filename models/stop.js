01a4f1d55039368f9409cb8f1799ad06b11cfd61
Hermes Espinola <hermes.espinola@gmail.com>
2017-10-28 20:06:38 -0500

    Update sequelize

--- models/stop.js
+++ models/stop.js
@@ -5,7 +5,8 @@ module.exports = function(sequelize, DataTypes) {
     name: {type: DataTypes.STRING, primaryKey: true},
     time: {type: DataTypes.STRING, primaryKey: true},
     createdAt: {type: DataTypes.DATE, defaultValue: sequelize.fn('now')},
-    updatedAt: {type: DataTypes.DATE, defaultValue: sequelize.fn('now')}
+    updatedAt: {type: DataTypes.DATE, defaultValue: sequelize.fn('now')},
+    trigger: {type: DataTypes.GEOMETRY('LINE')}
   });
 
   Stop.associate = function(models) {
