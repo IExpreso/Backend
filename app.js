0c05539dfcb7c6f030c934fcc450bd03be7e69dc
Hermes Espinola <hermes.espinola@gmail.com>
2017-11-01 11:28:09 -0600

    Add token header to allow headers

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
 	res.header("Access-Control-Allow-Credentials", "true");
 	next();
 });
