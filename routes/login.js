599aafa9a57dbe3f9ae2d6da275524a785b55631
Hermes Espinola <hermes.espinola@gmail.com>
2017-11-01 19:14:02 -0600

    Hash passwords

--- routes/login.js
+++ routes/login.js
@@ -3,25 +3,26 @@
 const models  = require('../models');
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const router  = express.Router();
 const env = process.env.NODE_ENV || 'development';
 const secret  = require(__dirname + './../config/config.json')[env]['secret'];
 
 router.post('/', (req, res) => {
-  console.log(req.body);
   models.User.findById(req.body.email).then(user => {
     if (!user) {
       return res.status(401).json({ error: 'invalid user' });
-    } else if (req.body.password === user.get('password')) {
-      let token = jwt.sign({ user: user.get('email') }, secret, { expiresIn: '48h' });
-      return res.status(200).json({
-        token: token,
-        user: user.email
-      });
-    } else {
-      return res.status(401).json({ error: 'invalid password' });
     }
+    bcrypt.compare(req.body.password, user.get('password'), (err, hashed) => {
+      if (hashed) {
+        let token = jwt.sign({ user: user.get('email') }, secret, { expiresIn: '48h' });
+        return res.status(200).json({
+          token: token,
+          user: user.email
+        });
+      } else return res.status(401).json({ error: 'invalid password' });
+    });
   }).catch(err => {
     return res.status(500).json({ error: err });
   });
--- routes/signup.js
+++ routes/signup.js
@@ -3,6 +3,7 @@
 var models  = require('../models');
 var express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 var router  = express.Router();
 const env = process.env.NODE_ENV || 'development';
@@ -14,23 +15,32 @@ router.post('/', (req, res, next) => {
       return res.status(404).json({ error: 'No such id: ' + req.body.id });
     if (student.UserEmail)
       return res.status(422).json({ error: 'Student already registered' });
-    models.User.create({
-      email: req.body.email,
-      password: req.body.password
-    }).then(user => {
-      user.setStudent(student, {save: true});
-      let token = jwt.sign({
-        user: user.get('email'),
-        role: user.get('role')
-      }, secret, { expiresIn: '48h' });
-      return res.status(201).json({
-        token: token,
-        user: user.email
-      });
-    }).catch(err => {
-      console.log(err);
-      return res.status(409).json({error: 'Email already in use'});
+
+    // hash password before storing it
+    bcrypt.hash(req.body.password, 10, (err, hashed) => {
+      if (hashed) {
+        models.User.create({
+          email: req.body.email,
+          password: hashed
+        }).then(user => {
+          user.setStudent(student, {save: true});
+          let token = jwt.sign({
+            user: user.get('email'),
+            role: user.get('role')
+          }, secret, { expiresIn: '48h' });
+          return res.status(201).json({
+            token: token,
+            user: user.email
+          });
+        }).catch(err => {
+          console.log(err);
+          return res.status(409).json({error: 'Email already in use'});
+        });
+      } else {
+        return res.status(500).json({error: 'Could not create user, Please try again later.'})
+      }
     });
+
   }).catch(err => {
     return res.status(500).json({error: err});
   });
